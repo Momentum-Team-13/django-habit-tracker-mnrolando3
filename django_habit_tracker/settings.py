5b75d2d8bc20dcba13bc1a1ea59ddb38b85a66df
Metta Rolando <mnrolando3@gmail.com>
2022-07-12 11:12:50 -0400

    Worked on postgres set up

--- django_habit_tracker/settings.py
+++ django_habit_tracker/settings.py
@@ -83,10 +83,8 @@ WSGI_APPLICATION = 'django_habit_tracker.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': env.db(),
+    
 }
 
 
